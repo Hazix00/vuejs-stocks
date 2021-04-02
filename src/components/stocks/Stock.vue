c659649542eb59170f5e7d6ff85206681a9b613d
Hazix00 <taoussi.hamza.dev@gmail.com>
2021-04-02 10:13:30 +0200

    add buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,17 +3,22 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    NAME
-                    <small>(Price : PRICE)</small>
+                    {{ stock.name }}
+                    <small>(Price : {{ stock.price }})</small>
                     <div class="panel-body">
                         <div class="pull-left">
                             <input 
                             type="number" 
                             class="form-controle" 
-                            placeholder="Quantity">
+                            placeholder="Quantity"
+                            v-model.number="quantity">
                         </div>
                         <div class="pull-right">
-                            <button class="btn btn-success">Buy</button>
+                            <button 
+                            class="btn btn-success"
+                            @click="buyStock"
+                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                            >Buy</button>
                         </div>
                     </div>
                 </h3>
@@ -21,3 +26,25 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        buyStock() {
+            const order={
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log(order)
+            this.quantity=0
+        }
+    }
+}
+</script>
