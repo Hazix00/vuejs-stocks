5da1d8a9402940e8e39cbe34c11ec9980adf2e55
Taoussi Hamza <taoussi.hamza.dev@gmail.com>
2021-04-02 21:28:45 +0100

    setup firebase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-
-import { routes } from './routes';
-import store from './store/store';
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 
-Vue.config.productionTip = false;
-Vue.use(VueRouter);
+import { routes } from './routes'
+import store from './store/store'
+
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vue-complete-70fcd-default-rtdb.firebaseio.com/'
 
 const router = new VueRouter({
   mode: 'history',
@@ -15,7 +20,7 @@ const router = new VueRouter({
 });
 
 Vue.filter('currency', function (value) {
-  return '$' + parseFloat(value).toFixed(2);
+  return '$' + parseFloat(value).toFixed(2)
 });
 
 new Vue({
