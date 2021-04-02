1d95d0ef313b92b66f999796e05f65bd9e2e11f7
Hazix00 <taoussi.hamza.dev@gmail.com>
2021-04-02 11:34:11 +0200

    portfolio stock

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,55 @@
 ﻿<template>
-    <h1>The Portfolio - Stock Component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>(Price : {{ stock.price }}) | Quantity : {{ stock.quantity }}</small>
+                    <div class="panel-body">
+                        <div class="pull-left">
+                            <input 
+                            type="number" 
+                            class="form-controle" 
+                            placeholder="Quantity"
+                            v-model.number="quantity">
+                        </div>
+                        <div class="pull-right">
+                            <button 
+                            class="btn btn-success"
+                            @click="sellStock"
+                            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                            >Buy</button>
+                        </div>
+                    </div>
+                </h3>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import mapActions from 'vuex'
+
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        ...mapActions([
+            'sellStock'
+        ]),
+        sellStock() {
+            const order={
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log(order)
+            this.sellStock()
+        }
+    }
+}
+</script>
